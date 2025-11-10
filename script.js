74e6f299beaa2ec4fca865fef8fa41472127e76f
joaomiguell1108 <joaomiguelmorais1108@gmail.com>
2025-11-10 16:34:52 -0300

    mudar

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const titulo = document.getElementById("texto").innerText;
-console.log(titulo)
-alert("O texto é:"+ titulo)
+const campoTexto = document.getElementById('campoTexto');
+campoTexto.addEventListener('input', (e)=>{
+    console.log(e.target.value);
+}); 
+
+const caixa = document.getElementById('caixa');
+//caixa.innerHTML = '<h2>Texto adicionadio pelo java</h2>';
+
+const mensagem = document.getElementById('mensagem');
+const btn = document.getElementById('btn');
+btn.addEventListener('click', (e)=>{
+    const valor = campoTexto.value;
+    mensagem.textContent = `Olá ${valor}`;
+    
+});
